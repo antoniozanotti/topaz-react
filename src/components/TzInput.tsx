1c96de9eabcab7056bf3df12fb4413ae1bbadbb3
Antonio Zanotti <antoniorzanotti@gmail.com>
2024-02-08 11:56:34 -0300

    feat: textarea and input components

--- src/components/TzInput.tsx
+++ src/components/TzInput.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 
 export interface TzInputProps {
-  type?: 'text' | 'email'
-  variant?: 'accent' | 'primary' | 'secondary' | 'negative' | 'dark' | 'light';
+  type?: 'text' | 'email';
   className?: string;
 }
 
-export const TzInput = ({
-  
-}: TzInputProps) => {
+export const TzInput = ({ type = 'text', className = '' }: TzInputProps) => {
+  /* input classes */
+  let inputOtherClasses =
+    'rounded text-primary dark:text-dark-primary bg-secondary dark:bg-dark-secondary';
+
+  // size
+  let sizeClasses =
+    'h-[28px] sm:h-[38px] lg:h-[48px] px-[10px] sm:px-[14px] lg:px-[20px]';
   return (
-    <span>TzInput</span>
+    <input
+      type={type}
+      className={`${inputOtherClasses} ${sizeClasses} ${className}`}
+    />
   );
 };
--- src/components/TzTextarea.tsx
+++ src/components/TzTextarea.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
 export interface TzTextareaProps {
-  rows?: number
-  variant?: 'accent' | 'primary' | 'secondary' | 'negative' | 'dark' | 'light';
+  rows?: number;
   className?: string;
 }
 
-export const TzTextarea = ({
-  
-}: TzTextareaProps) => {
+export const TzTextarea = ({ rows = 7, className = '' }: TzTextareaProps) => {
+  /* input classes */
+  let inputOtherClasses =
+    'rounded text-primary dark:text-dark-primary bg-secondary dark:bg-dark-secondary';
+
+  // size
+  let sizeClasses =
+    'h-[177px] px-[10px] sm:px-[14px] lg:px-[20px] py-[7px] sm:py-[10px] lg:py-[12px]';
+
   return (
-    <span>TzTextarea</span>
+    <textarea
+      rows={rows}
+      className={`${inputOtherClasses} ${sizeClasses} ${className}`}
+    ></textarea>
   );
 };

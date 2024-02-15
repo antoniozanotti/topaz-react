ebc2e3a746cd8e2bbdaec0925896b92e9c84dcb9
Antonio Zanotti <antoniorzanotti@gmail.com>
2024-02-15 17:00:57 -0300

    feat: add ref to input and textarea

--- src/components/TzInput.tsx
+++ src/components/TzInput.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { useVariantClasses, useFocusClasses } from './useStyles';
 
 export interface TzInputProps {
@@ -7,33 +7,39 @@ export interface TzInputProps {
   style?: 'fill' | 'outline';
   required?: boolean;
   className?: string;
+  name?: string;
 }
 
-export const TzInput = ({
-  type = 'text',
-  variant = 'accent',
-  style = 'fill',
-  required = true,
-  className = '',
-}: TzInputProps) => {
-  /* input classes */
-  let inputOtherClasses = 'rounded';
+export const TzInput = forwardRef<HTMLInputElement, TzInputProps>(
+  ({
+    type = 'text',
+    variant = 'accent',
+    style = 'fill',
+    required = true,
+    className = '',
+    name = ''
+  }: TzInputProps, ref) => {
+    /* input classes */
+    let inputOtherClasses = 'rounded';
 
-  // focus
-  let focusClasses = useFocusClasses();
+    // focus
+    let focusClasses = useFocusClasses();
 
-  // size
-  let sizeClasses =
-    'h-[28px] sm:h-[38px] lg:h-[48px] px-[10px] sm:px-[14px] lg:px-[20px]';
+    // size
+    let sizeClasses =
+      'h-[28px] sm:h-[38px] lg:h-[48px] px-[10px] sm:px-[14px] lg:px-[20px]';
 
-  // variants and styles
-  let variantClasses = useVariantClasses(variant, style);
+    // variants and styles
+    let variantClasses = useVariantClasses(variant, style);
 
-  return (
-    <input
-      type={type}
-      className={`${inputOtherClasses} ${focusClasses} ${sizeClasses} ${variantClasses} ${className}`}
-      required={required}
-    />
-  );
-};
+    return (
+      <input
+        type={type}
+        className={`${inputOtherClasses} ${focusClasses} ${sizeClasses} ${variantClasses} ${className}`}
+        required={required}
+        name={name}
+        ref={ref}
+      />
+    );
+  }
+);
--- src/components/TzTextarea.tsx
+++ src/components/TzTextarea.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { useVariantClasses, useFocusClasses } from './useStyles';
 
 export interface TzTextareaProps {
@@ -7,34 +7,40 @@ export interface TzTextareaProps {
   style?: 'fill' | 'outline';
   required?: boolean;
   className?: string;
+  name?: string;
 }
 
-export const TzTextarea = ({
-  rows = 7,
-  variant = 'accent',
-  style = 'fill',
-  required = true,
-  className = '',
-}: TzTextareaProps) => {
-  /* input classes */
-  let inputOtherClasses =
-    'rounded';
+export const TzTextarea = forwardRef<HTMLTextAreaElement, TzTextareaProps>(
+  ({
+    rows = 7,
+    variant = 'accent',
+    style = 'fill',
+    required = true,
+    className = '',
+    name = ''
+  }: TzTextareaProps, ref) => {
+    /* input classes */
+    let inputOtherClasses =
+      'rounded';
 
-  // focus
-  let focusClasses = useFocusClasses();
+    // focus
+    let focusClasses = useFocusClasses();
 
-  // size
-  let sizeClasses =
-    'h-[177px] px-[10px] sm:px-[14px] lg:px-[20px] py-[7px] sm:py-[10px] lg:py-[12px]';
-  
-  // variants and styles
-  let variantClasses = useVariantClasses(variant, style);
+    // size
+    let sizeClasses =
+      'h-[177px] px-[10px] sm:px-[14px] lg:px-[20px] py-[7px] sm:py-[10px] lg:py-[12px]';
+    
+    // variants and styles
+    let variantClasses = useVariantClasses(variant, style);
 
-  return (
-    <textarea
-      rows={rows}
-      className={`${inputOtherClasses} ${focusClasses} ${sizeClasses} ${variantClasses} ${className}`}
-      required={required}
-    ></textarea>
-  );
-};
+    return (
+      <textarea
+        rows={rows}
+        className={`${inputOtherClasses} ${focusClasses} ${sizeClasses} ${variantClasses} ${className}`}
+        required={required}
+        name={name}
+        ref={ref}
+      ></textarea>
+    );
+  }
+);

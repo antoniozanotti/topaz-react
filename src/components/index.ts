3506076b7ad5982f736835a346b390010a9384ec
Antonio Zanotti <antoniorzanotti@gmail.com>
2024-02-27 11:49:28 -0300

    refactory: removed TzPicutre component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,5 @@ export * from "./TzButton";
 export * from "./TzHeading";
 export * from "./TzIcon";
 export * from "./TzInput";
-export * from "./TzPicture";
 export * from "./TzTextarea";
 export * from "./TzToggleTheme";

296c9b4513e6934cfc7a82918939066138ce64fc
Antonio Zanotti <antoniorzanotti@gmail.com>
2024-10-11 16:53:29 -0300

    refactor: TzToggleTheme component
    - interface in new file
    - remove comments

--- src/components/TzToggleTheme/TzToggleTheme.tsx
+++ src/components/TzToggleTheme/TzToggleTheme.tsx
@@ -1,33 +1,30 @@
 'use client';
 import React from 'react';
+import { TzToggleThemeInterface } from './TzToggleThemeInterface';
 import { TzIcon } from '../TzIcon/TzIcon';
-
-export interface TzToggleThemeProps extends React.ComponentProps<'button'> {
-  className?: string;
-}
 
 export function TzToggleTheme({
   className = '',
   ...props
-}: TzToggleThemeProps) {
-  let buttonBackground = 'bg-secondary dark:bg-dark-secondary';
-  let buttonSize = 'w-[104px] h-[38px] lg:w-[132px] lg:h-[48px]';
-  let buttonOther = 'rounded-full cursor-pointer relative flex justify-around';
+}: TzToggleThemeInterface) {
+  const buttonBase = 'rounded-full cursor-pointer relative flex justify-around';
+  const buttonBackground = 'bg-secondary dark:bg-dark-secondary';
+  const buttonSize = 'w-[104px] h-[38px] lg:w-[132px] lg:h-[48px]';
 
-  let spanAnimation = 'transform duration-300';
-  let spanBackground = 'bg-secondary-2 dark:bg-dark-secondary-2';
-  let spanOther = 'block rounded-full absolute';
-  let spanSize = 'w-[60px] h-[32px] lg:w-[77px] lg:h-[42px]';
-  let spanPosition = 'self-center left-[40px] dark:left-[3px] lg:left-[52px]';
+  const spanBase = 'block rounded-full absolute';
+  const spanAnimation = 'transform duration-300';
+  const spanBackground = 'bg-secondary-2 dark:bg-dark-secondary-2';
+  const spanSize = 'w-[60px] h-[32px] lg:w-[77px] lg:h-[42px]';
+  const spanPosition = 'self-center left-[40px] dark:left-[3px] lg:left-[52px]';
 
   return (
     <button
-      className={`${buttonBackground} ${buttonSize} ${buttonOther} ${className}`}
+      className={`${buttonBase} ${buttonBackground} ${buttonSize} ${className}`}
       {...props}
       aria-label="Toggle Theme"
     >
       <span
-        className={`${spanAnimation} ${spanBackground} ${spanOther} ${spanSize} ${spanPosition}`}
+        className={`${spanBase} ${spanAnimation} ${spanBackground} ${spanSize} ${spanPosition}`}
       />
       <TzIcon
         iconName="MoonIcon"
---
+++ src/components/TzToggleTheme/TzToggleThemeInterface.ts
@@ -0,0 +1,3 @@
+export interface TzToggleThemeInterface extends React.ComponentProps<'button'> {
+  className?: string;
+}
